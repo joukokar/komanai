f4e385970cd49771cfcdb3923d6dd1050083eb0f
Jouko Karvonen <joukokar@gmail.com>
2015-02-08 08:20:28 +0900

    parse imagenames and connect to markers

--- client/app/event/ymap.directive.js
+++ client/app/event/ymap.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('komanaiApp')
-  .directive("ymap", function(unity) {
+  .directive("ymap", function(unity, socket) {
     return {
       restrict: "A",
       link: function (scope, elem, attrs) {
@@ -13,8 +13,9 @@ angular.module('komanaiApp')
              dragging : false
            }
          });
+        scope.ymap = ymap;
 
-        ymap.drawMap(new Y.LatLng(35.712711, 139.804622), 17, Y.LayerSetId.NORMAL);
+        ymap.drawMap(new Y.LatLng(35.712711, 139.804622), 16, Y.LayerSetId.NORMAL);
 
         var mapdata = [
           [1,2,3,4,2,4,2,1,3,2,0,0,2,5,4,2,3,4,2],
@@ -32,14 +33,14 @@ angular.module('komanaiApp')
         //between these
         //35.709261, 139.791673
         //35.717903, 139.815319
-        scope.latToY = function(lat) {
+        scope.latToZ = function(lat) {
           var val = (lat - 35.709261) / (35.717903 - 35.709261) * 5.65;
-          console.log("lat", val);
+          console.log("z", val);
           return val;
         }
         scope.lngToX = function(lng) {
           var val = (lng - 139.791673) / (139.815319 - 139.791673 * 13.65);
-          console.log("lng", val);
+          console.log("x", val);
           return val;
         }
         var centerLat = 35.712711;
@@ -49,19 +50,79 @@ angular.module('komanaiApp')
             if(mapdata[y][x] != 0) {
               var strokeStyle = new Y.Style(colors[mapdata[y][x]], null, 0.0);
               var fillStyle   = new Y.Style(colors[mapdata[y][x]], null, 0.2);
-              var circle = new Y.Circle(new Y.LatLng(centerLat-y/1000+0.003, centerLng+x/1000-0.01), new Y.Size(0.05, 0.05), {
+              var circle = new Y.Circle(
+                new Y.LatLng(centerLat-y/1000+0.003, centerLng+x/1000-0.01),
+                new Y.Size(0.05, 0.05),
+                {
                   unit:"km",
                   strokeStyle: strokeStyle,
                   fillStyle: fillStyle
               });
               circle.bind('click', function(latlng){
                   console.log(latlng, this);
-                  unity.moveToPosition(scope.latToY(this.latlng.Lat), scope.lngToX(this.latlng.Lon))
+                  unity.moveToPosition(scope.latToZ(this.latlng.Lat), 1, scope.lngToX(this.latlng.Lon))
               });
               ymap.addFeature(circle);
             }
           }
         }
+
+
+        // parse name and return {lat:, lng:}
+        function parseImageName(name) {
+          var lat, lng;
+
+          var parts = name.match(/(\d+)_(\d+)_(\d+).*/);
+          //console.log(name, parts);
+          lat = parseInt(parts[1])/10000000;
+          lng = parseInt(parts[2])/10000000;
+
+          // TODO: capture timestamp
+          //var time = moment().year(parts[3].splice(0,4))
+          //console.log(time);
+          return {
+            lat: lat,
+            lng: lng
+          }
+        }
+
+        function hasExistingMarker(location) {
+          var matchingMarkers = _.select(scope.markers, function(m) {
+            return location.lat == m.lat && location.lng == m.lng
+          });
+          return matchingMarkers.length > 0;
+        }
+
+        function addMarker(name, location) {
+            var marker = new Y.Marker(new Y.LatLng(location.lat, location.lng));
+            location.name = name;
+            location.marker = marker;
+            scope.markers.push(location)
+            scope.ymap.addFeature(marker);
+
+            marker.bind("click", function() {
+              console.log("clicked", location.name);
+              unity.showingImage = scope.images.indexOf(location.name)
+            });
+
+        }
+
+        scope.markers = [];
+
+        socket.socket.on('image:list', function(res) {
+          var img;
+          scope.images = res;
+          for(var i=0; i<scope.images.length; i++) {
+            img = scope.images[i];
+            var parsedLocation = parseImageName(img);
+
+            // add only if does not exist already
+            if(!hasExistingMarker(parsedLocation)) {
+              console.log("adding", parsedLocation);
+              addMarker(img, parsedLocation);
+            }
+          }
+        });
 
         ymap.bind('click', function(latlng){
             console.log(latlng.toString());
--- client/app/unity/unity.service.js
+++ client/app/unity/unity.service.js
@@ -5,9 +5,14 @@ angular.module('komanaiApp')
     // AngularJS will instantiate a singleton by calling "new" on this function
     var service = {};
     service.u = new UnityObject2();
+    service.currentPos = {};
 
-    service.moveToPosition = function(x,y) {
-      service.u.getUnity().SendMessage("MainCamera", "MoveToPosition", x + "," + y);
+    // TODO: does not belong here
+    service.showingImage = -1;
+
+    service.moveToPosition = function(x,y,z) {
+      service.currentPos = {x:x, y:y, z:z}
+      service.u.getUnity().SendMessage("MainCamera", "MoveToPosition", x + "," + y + "," + z);
     }
     return service;
   });
